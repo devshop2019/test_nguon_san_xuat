f230d44f42d19d78c6793017b14e31e2c97d9cc2
vietduino <vietduino@gmail.com>
2019-07-31 17:52:44 +0700

    PCB, ino

---
+++ prototype/KiemTraNguon_prototype/KiemTraNguon_prototype.ino
@@ -0,0 +1,28 @@
+/*
+  I. Yeu cau KT:
+   1) Do thong mach cac chan: Vin - GND - 5V - 3V3   => can 4 chan analog
+    - Vin - GND
+    - Vin - 5V
+    - 5V - GND
+    - 5V - 3v3
+
+   2) Do Dien Ap
+    - Out 5V
+    - Out 3V3
+    - Out Usb
+
+   3) Thong bao loi qua Serial.
+
+  II. Nguyen ly hoat dong:
+   1) kiem tra thong mach Vin va GND: Vin dc Pull-UP, kich 0V vao GND. Neu Vin = 0V => Chap
+   2) Kiem tra thong mach Vin va 5V:  Vin dc Pull-UP, kich 0V vao 5V, Ngat GND. Neu Vin = 0V => Chap
+*/
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
