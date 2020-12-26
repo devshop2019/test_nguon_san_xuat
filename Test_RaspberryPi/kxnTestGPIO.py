b848de319f5caca97f9ab1dc5866f2055244da2c
devshop2019 <devshop2019@gmail.com>
2020-12-26 16:08:57 +0700

    Full test SPIO For Raspberry OK

--- Test_RaspberryPi/kxnTestGPIO.py
+++ Test_RaspberryPi/kxnTestGPIO.py
@@ -1,21 +1,25 @@
+#https://github.com/devshop2019/test_nguon_san_xuat/tree/master/Test_RaspberryPi
+# Neu chan nao bi loi se bao len
 from array import *
 import RPi.GPIO as gpio
 import time
 
 pin = 5
 array1 = array('i', [10,20,30,40,50])
+kxnErrcode = 2
 
 def testPullup():
-    
+    global kxnErrcode
     for cf in range (1, 26):
         gpio.setup(cf, gpio.IN, pull_up_down=gpio.PUD_UP)
     
     for cf in range (1, 26):
         if gpio.input(cf) == 0:
             print("pin " + str(cf) + "pull UP fail!")
+            kxnErrcode = 1
             
 def testPulldown():
-    
+    global kxnErrcode
     for cf in range (1, 26):
         gpio.setup(cf, gpio.IN, pull_up_down=gpio.PUD_DOWN)
     
@@ -24,19 +28,22 @@ def testPulldown():
             gpio.setup(cf, gpio.OUT)
             gpio.output(cf, gpio.LOW)
             if gpio.input(cf) == 1:
-                
                 gpio.setup(cf, gpio.IN, pull_up_down=gpio.PUD_DOWN)
                 print("pin " + str(cf) + "pull DOWN fail!")
+                kxnErrcode = 1
+                print(kxnErrcode)
 
 gpio.setmode(gpio.BCM)
 #gpio.setup(6, gpio.IN, pull_up_down=gpio.PUD_UP)
 #gpio.setup(pin, gpio.IN, pull_up_down=gpio.PUD_UP)
 gpio.setwarnings(False)
+print("first errcode " + str(kxnErrcode))
 try:
     while True:
         testPullup()
         testPulldown()
-        
+        if kxnErrcode == 2:
+            print("All Pin Test OK " + str(kxnErrcode))
         time.sleep(1)
         #gpio.output(18, gpio.HIGH)
         #passcode = raw_input("What is pi?: ")
